1233bd6ef82568ab760fc9e0935dc78b0f893221
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-13 03:13:56 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,12 @@
 class Post 
-  attr_accessor :title, :author
+  attr_accessor :title, :auther 
+  
+  def initialize(title)
+    @title = title 
+  end 
+  
+  def author_name
+    @auther.name 
+  end
+  
 end
