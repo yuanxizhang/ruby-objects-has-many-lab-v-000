c2f848ae2ae555b0bd47068afda977fdfe708df6
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-13 03:41:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,17 @@
 class Artist 
   attr_accessor :name, :songs 
   
-  @@all_songs = []
+  @@total_songs = 0 
   
   def initialize(name = nil)
     @name = name 
     @songs = []
+    @@total_songs += 1
   end 
   
   def self.song_count
-    @@all_songs.inject(:& +@songs.count)
+    total = 0 
+    @@all_songs.each {|songs| total += songs.count
   end 
   
   def add_song(song)
