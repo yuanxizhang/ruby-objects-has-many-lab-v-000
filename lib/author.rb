3bbaa88d61b85d5d374052ba0d305796040d494c
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-13 03:05:44 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author  
+  attr_accessor :name, :posts 
+  
+  def initialize(name = nil)
+    @name = name 
+    @songs = []
+  end 
+  
+  def song_count
+    @songs.length 
+  end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    song.artist = self
+    song 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+end
